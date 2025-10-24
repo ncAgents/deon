72fafef6bc5fd168f87a3f2e6319191983309d09
Deon62 <orinadeon@gmail.com>
2025-10-24 20:24:42 +0300

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { HelmetProvider } from 'react-helmet-async'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </React.StrictMode>,
 )
